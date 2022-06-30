d8eb42e496c8710c56dd784e7c2344f7242f824b
Dahye Ji <daaahailey@gmail.com>
2022-06-30 23:02:56 +0900

    Add: reset 스타일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Global } from '@emotion/react'
 
 import App from './App'
+import reset from './style/reset'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
     <App />
   </React.StrictMode>,
 )
