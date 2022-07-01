e52f141ac59e5b5f7b72765e3dc83ac1f79e5ceb
Dahye Ji <daaahailey@gmail.com>
2022-07-02 00:41:05 +0900

    Add: images/index.ts 사용할 이미지 추가, @types/global/index.d.ts 이미지 확장자(png) 선언

---
+++ src/images/index.ts
@@ -0,0 +1,5 @@
+import TripleLogoImg from './triple2x.png'
+import PlayStoreImg from './play-store2x.png'
+import AppStoreImg from './badge-apple4x.png'
+
+export { TripleLogoImg, PlayStoreImg, AppStoreImg }
